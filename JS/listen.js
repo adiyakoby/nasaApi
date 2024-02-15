93ea7eced43ea7c90943ad5fb96670a478c3b6c2
adi65 <adija@edu.hac.ac.il>
2024-02-15 22:03:31 +0200

    Fixed Home page, changed texts and added spinner. added dom listener and dom element finders.

---
+++ JS/listen.js
@@ -0,0 +1,31 @@
+"use strict";
+
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+
+        //Dom content and buttons
+
+        //nav buttons
+        const HomeButton = document.getElementById("home-page");
+        const SavedImagesButton = document.getElementById("saved-images");
+
+
+        //form content
+        const dateFormat = document.getElementById("");
+        const DateSelection = document.getElementById("");
+        const roverSelection = document.getElementById("");
+        const cameraSelection = document.getElementById("");
+        const searchButton = document.getElementById("search-Button");
+        const ClearButton = document.getElementById("clear-Button");
+
+        //images container
+        const imagesContainer = document.getElementById("images-container");
+
+
+
+
+
+    });
+
+
+})();
